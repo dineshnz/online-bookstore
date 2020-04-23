aa17bc6fed0c8a0c4686710c945bfccbc74e5827
Dinesh Karki <dineshkrk46@gmail.com>
2020-04-23 13:29:52 -0500

    added angular project to the directory

--- online-book-store/src/main/java/com/dinesh/onlinebookstore/repository/BookRepository.java
+++ online-book-store/src/main/java/com/dinesh/onlinebookstore/repository/BookRepository.java
@@ -1,9 +1,11 @@
 package com.dinesh.onlinebookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.dinesh.onlinebookstore.entity.Book;
 
+@CrossOrigin(origins="http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
