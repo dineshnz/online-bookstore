fce14e168c2ff3e2119b0e5bf0cf436598f80a6c
Dinesh Karki <dineshkrk46@gmail.com>
2020-04-24 14:12:59 -0500

    added angular project to the git hub

--- online-book-store/src/main/java/com/dinesh/onlinebookstore/repository/BookCategoryRepository.java
+++ online-book-store/src/main/java/com/dinesh/onlinebookstore/repository/BookCategoryRepository.java
@@ -2,9 +2,11 @@ package com.dinesh.onlinebookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.dinesh.onlinebookstore.entity.BookCategory;
 
+@CrossOrigin(origins="http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "bookCategory", path="book-category")
 public interface BookCategoryRepository extends JpaRepository<BookCategory, Long> {
 
